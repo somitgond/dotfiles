29141b798c6ad3b51ec34cfeb906728456891789
Somit Gond <somitgond2@gmail.com>
2025-02-24 19:27:12 +0530

    emacs update

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -8,7 +8,8 @@
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
 
 ;; set custom file
-(setq custom-file "~/.emacs.custom.el")
+(setq custom-file "~/.config/emacs/custom.el")
+(setq python-shell-virtualenv-path "~/.local/python3/")
 
 ;; adding melpa
 (require 'package)
@@ -16,20 +17,31 @@
 (package-initialize)
 
 ;; setting custom fonts
-(add-to-list 'default-frame-alist `(font . "Iosevka-14"))
+;;(add-to-list 'default-frame-alist `(font . "Iosevka-14"))
+(add-to-list 'default-frame-alist `(font . "JetBrains Mono-13"))
 
 ;; UI customizations
+
+;; no tool bar
+(push '(tool-bar-lines . 0) default-frame-alist)
+
+;; no menu bar
 (menu-bar-mode 0)
-(tool-bar-mode 0)
+
+;; stop creating ~ files
+(setq make-backup-files nil)
+
 (scroll-bar-mode 0)
 (blink-cursor-mode 0)
 (setq inhibit-startup-screen t)
 ;;(setq initial-major-mode 'org-mode)
 (setq initial-scratch-message nil)
-(defun display-startup-echo-area-message ()
-  (message "Leave: C-x C-c  Help: C-h C-h"))
 
-(setq display-line-numbers 'relative) 
+;;(setq display-line-numbers-type 'relative) 
+(global-display-line-numbers-mode 1)
+
+;; word wrap 
+(global-visual-line-mode t)
 
 ;; don't suspend witn C-z
 (global-unset-key (kbd "C-z"))
@@ -39,9 +51,10 @@
 (setq dired-auto-revert-buffer 1)
 ;; auto refresh dired when file changes
 (add-hook 'dired-mode-hook 'auto-revert-mode)
-(ido-mode 1)
 
-(global-display-line-numbers-mode)
+;; ido mode
+(ido-mode 1)
+(ido-everywhere 1)
 
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
@@ -58,10 +71,7 @@
 ;; installing packages
 ;;(require 'org)
 ;;(require 'smex)
-(require 'tree-sitter)
-(require 'tree-sitter-langs)
 
-;; for git
 (use-package magit
   :bind ("C-x g" . magit-status))
 
@@ -77,36 +87,49 @@
   :config
   (smartparens-global-mode 1)
   )
-;; treesitter
+;; treesitter: emacs compiled with --tree-sitter flag
 (add-hook 'go-mode-hook #'tree-sitter-mode)
 (add-hook 'python-mode-hook #'tree-sitter-mode)t
 (add-hook 'cc-mode-hook #'tree-sitter-mode)
 (add-hook 'c-mode-hook #'tree-sitter-mode)
 
-;; using eglot
-(add-hook 'go-mode-hook 'eglot-ensure)
-(add-hook 'c-mode-hook 'eglot-ensure)
-(add-hook 'python-mode-hook 'eglot-ensure)
-(add-hook 'cc-mode-hook 'eglot-ensure)
+;; using eglot as lsp: inbuilt in emacs
+;;(add-hook 'go-mode-hook 'eglot-ensure)
+;;(add-hook 'c-mode-hook 'eglot-ensure)
+;;(add-hook 'python-mode-hook 'eglot-ensure)
+;;(add-hook 'cc-mode-hook 'eglot-ensure)
 ;; which-key
-(use-package which-key
-  :config
-  (which-key-mode)
-  (setq which-key-idle-delay 0.3)
-  (which-key-setup-side-window-right-bottom)
-  )
 
 ;; smex key bindings
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+;;(global-set-key (kbd "M-x") 'smex)
+;;(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;;(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+
+;; amx
+;;(use-package amx
+;;  :config
+;;  (amx-mode 1))
+(require 'amx)
+(amx-mode 1)
+
+;; icomplete mode
+;;(require 'icomplete)
+;;(icomplete-mode 1)
 
 ;; org-mode key bindings
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
 
-;; custom keyboard shortcuts
+;; org mode stuff
+(setq org-hide-leading-stars t)
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode))
+
+;; Custom keyboard shortcuts
 
 
+
+;; load custom file
 (load-file custom-file)
